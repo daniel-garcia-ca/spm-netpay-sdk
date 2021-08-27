f1188a6a3e566fef90b2aaee7a556ed8cd67f623
Daniel Garcia <daniel.gar0814@gmail.com>
2021-08-27 01:06:38 -0500

    aa

--- Package.swift
+++ Package.swift
@@ -27,17 +27,17 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "NetPaySDK",
-                    url: "https://github.com/daniel-garcia-ca/xcframework-netpay/releases/tag/1.0.0/NetPaySDK.xcframework.zip",
+                    url: "https://github.com/daniel-garcia-ca/xcframework-netpay/releases/tag/1.0.1/NetPaySDK.xcframework.zip",
                     checksum: "ff0627b7ae1db0d7277363b7d68f001c6b572e08dcbe71591871434d2c65925b"
                 ),
         .binaryTarget(
                     name: "TMXProfiling",
-                    url: "https://github.com/daniel-garcia-ca/xcframework-tmx-profile/releases/tag/1.0.0/TMXProfiling.xcframework.zip",
+                    url: "https://github.com/daniel-garcia-ca/xcframework-tmx-profile/releases/tag/1.0.1/TMXProfiling.xcframework.zip",
                     checksum: "b4f285d7005da07e2224e43bb068bcdf17dcacb2e2e7583de0c9116ebb8fecd9"
                 ),
         .binaryTarget(
                     name: "TMXProfilingConnections",
-                    url: "https://github.com/daniel-garcia-ca/xcframework-tmx-profile-communication/releases/tag/1.0.0/TMXProfilingConnections.xcframework.zip",
+                    url: "https://github.com/daniel-garcia-ca/xcframework-tmx-profile-communication/releases/tag/1.0.1/TMXProfilingConnections.xcframework.zip",
                     checksum: "b71a3171edb8bbfcb928e5c9df19bca357b392a64b7ef213569062117e2724ec"
                 ),
     ]
