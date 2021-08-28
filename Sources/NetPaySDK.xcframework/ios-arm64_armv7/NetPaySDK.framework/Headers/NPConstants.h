4e6c89080b0f38534b7258d4a89caf76753170f7
Daniel Garcia <daniel.gar0814@gmail.com>
2021-08-27 20:03:20 -0500

    se agrega los xcframeworks en el paquete

---
+++ Sources/NetPaySDK.xcframework/ios-arm64_armv7/NetPaySDK.framework/Headers/NPConstants.h
@@ -0,0 +1,9 @@
+@import Foundation;
+
+
+typedef NSString * NPSourceTypeValue NS_EXTENSIBLE_STRING_ENUM;
+
+typedef NSString * NPSupportedCurrencyCode NS_EXTENSIBLE_STRING_ENUM;
+
+extern NPSupportedCurrencyCode const _Nonnull NPSupportedCurrencyCodeMXN;
+
---
+++ Sources/NetPaySDK.xcframework/ios-arm64_armv7/NetPaySDK.framework/Headers/NetPaySDK.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double NetPaySDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char NetPaySDKVersionString[];
+
+#import <NetPaySDK/NPConstants.h>
+
+#include<ifaddrs.h>
